50d86088f3f60ff10d4fabd4dc17745537d50a7f
arthur-moreau <arthurmoreauti@gmail.com>
2022-08-22 13:02:15 -0300

    protect open weather api key

--- src/providers/weather-provider.js
+++ src/providers/weather-provider.js
@@ -28,49 +28,47 @@ const WeatherProvider = ({ children }) => {
     icon: '',
   });
   
-  const apiKey = '15e4fc3930ee7d0cc5a48e8472313d03'
+  const apiKey = process.env.REACT_APP_OPEN_WEATHER_KEY
   
   // Function to get the current weather from API
 
-  const getWeather = (city) => {
+  function getWeather(city) {
 
     // Variable to define the temperature type
-    let units = ''
-    Globals.typeOfTemp === false ? units = 'metric' : units = 'imperial'
+    let units = '';
+    Globals.typeOfTemp === false ? units = 'metric' : units = 'imperial';
 
     // Requisition to open weather map
-
     api.get(`/weather?q=${city}&lang=pt_br&appid=${apiKey}&units=${units}`)
-    
-    .then(({ data: weatherData }) => {
-      setWeatherState((prevState) => ({
-        ...prevState,
-        city: weatherData.name.toUpperCase(),
-        description: weatherData.weather[0].description,
-        temp: Math.round(weatherData.main.temp),
-        max: Math.round(weatherData.main.temp_max),
-        min: Math.round(weatherData.main.temp_min),
-        icon: `http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`
-      }))
-    })
 
-    .catch((err) => {
-      if (err.request.status === 404) {
-        alert('Local não encontrado')
-        (setWeatherState((prevState) => ({
+      .then(({ data: weatherData }) => {
+        setWeatherState((prevState) => ({
           ...prevState,
-          searching: !prevState.searching
-        })))
-      }
-    })
+          city: weatherData.name.toUpperCase(),
+          description: weatherData.weather[0].description,
+          temp: Math.round(weatherData.main.temp),
+          max: Math.round(weatherData.main.temp_max),
+          min: Math.round(weatherData.main.temp_min),
+          icon: `http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`
+        }));
+      })
 
-    // function that permits React to render the results or get back if contain error
-    .finally (setWeatherState((prevState) => ({
-      ...prevState,
-      searching: !prevState.searching
-    })))
+      .catch((err) => {
+        if (err.request.status === 404) {
+          alert('Local não encontrado')(setWeatherState((prevState) => ({
+            ...prevState,
+            searching: !prevState.searching
+          })));
+        }
+      })
 
-  };
+      // function that permits React to render the results or get back if contain error
+      .finally(setWeatherState((prevState) => ({
+        ...prevState,
+        searching: !prevState.searching
+      })));
+
+  }
 
   // function to restart the app
 
